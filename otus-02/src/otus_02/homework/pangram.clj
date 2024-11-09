a20541f1bb07150173290b68893e72316e9e849d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2024-11-09 17:28:10 +0300

    complete pangram task

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,15 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
+(def alphabet-set (set "abcdefghijklmnopqrstuvwxyz"))
 
-(defn is-pangram [test-string])
+(defn normalize-str [input-str]
+  (-> input-str
+      string/trim
+      string/lower-case
+      (string/replace #"[^a-z]" "")))
 
+(defn is-pangram [test-string]
+  (=
+   alphabet-set
+   (set (normalize-str test-string))))
