efaa26dafc43f5180099885cf6e54c732ec3d9cd
Gregory Tereshko <gtrsh@users.noreply.github.com>
2024-11-10 19:57:24 +0300

    complete palindrome task

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,8 @@
 (ns otus-02.homework.palindrome
-  (:require [clojure.string :as string]))
+  (:require
+   [clojure.string :refer [reverse]]
+   [otus-02.homework.string-normalize :refer [normalize-str]]))
 
-
-(defn is-palindrome [test-string])
-
+(defn is-palindrome [test-string]
+  (let [input-string (normalize-str test-string)]
+    (= input-string (reverse input-string))))
